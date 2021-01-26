bcbffaea9b8143d084b8b5e0c0bc979ccad6c315
Alex Trombold <at354216@ohio.edu>
2021-01-26 10:31:53 -0500

    step 15

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Alex Trombold>
+ *        Date: <1/26/2020>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello git!!" << endl;
+    return 0;
+}// main
